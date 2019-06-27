ed90b67bc4515851e220d8d5865f8c34517ec946
efelantevolador <efelante985volador@gmail.com>
2019-06-26 23:03:49 -0400

    Mejora en diseño Base y perfil.

--- VIEW/Base-pag.php
+++ VIEW/Base-pag.php
@@ -5,26 +5,73 @@
         <title>TITULO</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../CSS/miestilo.css">
         <link rel="stylesheet" href="../CSS/bootstrap/css/bootstrap.min.css">
+        
     </head>
     <body>
-<!-- *******************************************| CONTENIDO |********************************************************-->
-    <header class="" style="background:red;">
-        <div class="container"><h1>Titulo</h1></div>
-    </header>
-    <div class="container-fluid" style="border:blue 1px solid;">
-        <div class="row"> <!--row: se utiliza para definir una tabla de posicionamiento donde utilizar despues las clases Col-xs-?  -->
-            <!--*************************| MENUS DE NAVEGACION IZQUIERDO |***********************************************************-->
-            <div class="col-xl-3" style="border:black solid 1px;">
-                <p>menus</p>
+<!-- ********************************************|1 CONTENIDO |*******************************************************************************************************************-->
+    <header>
+    <!-- Barra Navegacion -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="../VIEW/Base-pag.php">Trabaja a tu medida</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Info</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Desplegable
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Opción</a>
+                        <a class="dropdown-item" href="#">Otra opción</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Ultima Opción separada</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Déshabilitado</a>
+                    </li>
+                </ul>
+                <a class="nav-link" href="#">Iniciar Sesión <span class="sr-only">(current)</span></a>
+                <!-- *************** BUSCADOR
+                <form class="form-inline my-2 my-lg-0 disabled">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                -->
             </div>
-            <!--*************************| FIN MENUS DE NAVEGACION IZQUIERDO |*******************************************************-->
-            <div class="col-xl-9" style="border:black solid 1px;">
-                <p>contenido</p>
+        </div> 
+    <!-- Fin Barra Navegacion -->   
+    </nav>
+    </header>
+    
+    <div class="container-fluid borde-r">
+        <div class="row"> <!--row: se utiliza para definir una tabla de posicionamiento donde utilizar despues las clases Col-xs-?  -->
+            <!--*************************|2 MENU DE NAVEGACION IZQUIERDO |**********************************************************-->
+            <div class="col-xl-3">
+                
+            </div>
+            <!--*************************|2 FIN MENU DE NAVEGACION IZQUIERDO |******************************************************-->
+            <div class="col-xl-9">
+                <div class="">
+                <!--*****************|3 CONTENIDO INTERNO DE PAG|*****************************************************************-->
+                    
+                <!--*****************|3 FIN CONTENIDO INTERNO DE PAG|*************************************************************-->
+                </div>
             </div>
         </div>  
     </div>
-<!-- *******************************************| TERMINA EL CONTENIDO |*********************************************-->    
+<!-- *******************************************|1 TERMINA EL CONTENIDO |*********************************************************************************************************-->    
         <script src="../CSS/bootstrap/js/jquery-3.2.1.slim.min.js"></script>
         <script src="../CSS/bootstrap/js/popper.min.js"></script>
         <script src="../CSS/bootstrap/js/bootstrap.min.js"></script>
---
+++ VIEW/perfil.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Perfil Postulante/Empresa</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../CSS/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../CSS/miestilo.css">
+    </head>
+    <body>
+<!-- ********************************************|1 CONTENIDO |*******************************************************************************************************************-->
+    <header class="fondo-r justify-content-center">
+        
+    </header>
+    <div class="container-fluid" style="margin=0px;">
+        <div class="row"> <!--row: se utiliza para definir una tabla de posicionamiento donde utilizar despues las clases Col-xs-?  -->
+            <div class="col-xl-3 borde-b">
+            <!--*************************|2 MENU DE NAVEGACION IZQUIERDO |**********************************************************-->
+            hola
+            <!--*************************|2 FIN MENU DE NAVEGACION IZQUIERDO |******************************************************-->    
+            </div>
+            <div class="col-xl-9 borde-b">
+                <div class="container borde-r fondo-g">
+                <!--*****************|3 CONTENIDO INTERNO DE PAG|*****************************************************************-->
+                hola
+                <!--*****************|3 FIN CONTENIDO INTERNO DE PAG|*************************************************************-->
+                </div>
+            </div>
+        </div>  
+    </div>
+<!-- *******************************************|1 TERMINA EL CONTENIDO |*********************************************************************************************************-->    
+        <script src="../CSS/bootstrap/js/jquery-3.2.1.slim.min.js"></script>
+        <script src="../CSS/bootstrap/js/popper.min.js"></script>
+        <script src="../CSS/bootstrap/js/bootstrap.min.js"></script>
+        
+    </body>
+</html>
