7991923642203f5bbd68d9cdead03032a0188a81
efelantevolador <efelante985volador@gmail.com>
2019-06-26 22:03:16 -0400

    Esqueleto Diseño de pagina.

---
+++ PENDIENTES.md
@@ -0,0 +1,24 @@
+
+#Lista de pendientes
+
+#"Views Postulante/Empresa".
+#---------------------------------------------------------------------
+-Perfil Postulante 
+    Formulario que carge Informacion personal. (nombre, apellidos, fecha de nacimiento, Email, Telefono)
+    Tabla para agregar Estudios y Cursos completados.
+-Perfil empresa
+    Informacion de reprecentante de empresa.
+    Informacion de empresa.
+#---------------------------------------------------------------------
+-Perfil Funcional.
+    Clasificacion Discapacidades Movilidad (Motrices) PENDIENTE...
+#---------------------------------------------------------------------
+-Postulacion a Trabajos.
+    Listado de trabajos acordes a su discapacidad. FILTRO
+-Trabajos Postulados.
+    Lista de Postulaciones realizadas. (una breve vista o descripcion de postulacion)
+    opcion o boton para entrar a ver informacion completa.
+#---------------------------------------------------------------------
+-Crear Oferta Laboral.
+-Ver Ofertas Realizadas.
+#---------------------------------------------------------------------
---
+++ VIEW/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Index</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../CSS/bootstrap/css/bootstrap.min.css">
+    </head>
+    <body>
+<!-- *******************************************| CONTENIDO |********************************************************-->
+    <header class="" style="background:red;">
+        <div class="container"><h1>Titulo</h1></div>
+    </header>
+    <div class="container-fluid" style="border:blue 1px solid;">
+        <div class="row"> <!--row: se utiliza para definir una tabla de posicionamiento donde utilizar despues las clases Col-xs-?  -->
+            <!--*************************| MENUS DE NAVEGACION IZQUIERDO |***********************************************************-->
+            <div class="col-xl-3" style="border:black solid 1px;">
+                <p>Menus</p>
+                <p>Menus</p>
+                <p>Menus</p>
+                <p>Menus</p>
+                <p>Menus</p>
+                <p>Menus</p>
+            </div>
+            <!--*************************| FIN MENUS DE NAVEGACION IZQUIERDO |*******************************************************-->
+            <div class="col-xl-9" style="border:black solid 1px;">
+                <p>contenido</p>
+            </div>
+        </div>  
+    </div>
+<!-- *******************************************| TERMINA EL CONTENIDO |*********************************************-->    
+        <script src="../CSS/bootstrap/js/jquery-3.2.1.slim.min.js"></script>
+        <script src="../CSS/bootstrap/js/popper.min.js"></script>
+        <script src="../CSS/bootstrap/js/bootstrap.min.js"></script>
+        
+    </body>
+</html>
