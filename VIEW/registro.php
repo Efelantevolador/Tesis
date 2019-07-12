c2399636e8502c87d3610779c893b3c198a11d2c
efelantevolador <efelante985volador@gmail.com>
2019-07-11 22:35:06 -0400

    Interfas de Registro Terminado

--- VIEW/registro.php
+++ VIEW/registro.php
@@ -60,45 +60,174 @@
     
     <div class="container-fluid">
         <div class="row"> <!--row: se utiliza para definir una tabla de posicionamiento donde utilizar despues las clases Col-xs-?  -->
-            <div class="col-xl-3 borde-r">Izquierda</div>
-            <div class="col-xl-6" style="margin-top:50px;">
-                <h1 class="text-center">Formulario Registro</h1>
+            <div class="col-xl-3"></div>
+            <!--Centro de pagina -->
+            <div class="col-xl-6" style="margin-top:20px;border-left:dotted black 1px;border-right:dotted black 1px;border-top:dotted black 1px;background:lightblue;">
+                <h1 class="text-center" style="margin-top:20px;">Formulario Registro</h1>
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-12">
                             <h5>Informacion</h5>
                         </div>
-                        <div class="col-lx-4">
-                            <div class="input-group mb-1">
+                        <div class="col-xl-4">
+                            <div class="input-group mb-2">
                                 <div class="input-group-prepend">
-                                    <span class="input-group-text" id="basic-addon1">Nombre :</span>
+                                    <span class="input-group-text" id="basic-addon1">Rut :</span>
                                 </div>
-                                <input type="text" value="Nombre" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                                <input type="text" value="" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
                             </div>
                         </div>
-                        <div class="col-lx-4">
-                            <div class="input-group mb-1">
+                        <div class="col-xl-8"></div>
+                        <div class="col-xl-4">
+                                <div class="input-group mb-2">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon1">Nombre :</span>
+                                    </div>
+                                    <input type="text" value="" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                                </div>
+                        </div>
+                        <div class="col-xl-4">
+                            <div class="input-group mb-2">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text" id="basic-addon1">Apellido Paterno :</span>
                                 </div>
-                                <input type="text" value="Apellido" class="form-control" placeholder="" aria-label="apellidoP" aria-describedby="basic-addon1">
+                                <input type="text" value="" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
                             </div>
                         </div>
-                        <div class="col-lx-4">
-                            <div class="input-group mb-1">
+                        <div class="col-xl-4">
+                            <div class="input-group mb-2">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text" id="basic-addon1">Apellido Materno :</span>
                                 </div>
-                                <input type="text" value="Apellido" class="form-control" placeholder="" aria-label="apellidoM" aria-describedby="basic-addon1">
+                                <input type="text" value="" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
                             </div>
                         </div>
+                        <div class="col-xl-5">
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1">@email :</span>
+                                </div>
+                                <input type="text" value="" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        <div class="col-xl-4">
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1">Telefono :</span>
+                                </div>
+                                <input type="text" value="" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        <div class="col-xl-4"></div>
+                        <div class="col-xl-12">
+                            <h5>Fecha de nacimiento</h5>
+                        </div>
+                        <div class="col-xl-3">
+                            <div class="input-group mb-4">
+                                <div class="input-group-append">
+                                    <label class="input-group-text" for="inputGroupSelect02">Dia :</label>
+                                </div>
+                                <select class="custom-select" id="inputGroupSelect02">
+                                    <option selected disabled>-Dia-</option>
+                                    <?php  for($i=1;$i<=31;$i++) { echo "<option value='".$i."'>".$i."</option>"; } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-xl-3">
+                            <div class="input-group mb-4">
+                                <div class="input-group-append">
+                                    <label class="input-group-text" for="inputGroupSelect02">Mes :</label>
+                                </div>
+                                <select class="custom-select" id="inputGroupSelect02">
+                                    <option value="" selected disabled>- Mes -</option>
+                                    <option value="01">Enero</option>
+                                    <option value="02">Febrero</option>
+                                    <option value="03">Marzo</option>
+                                    <option value="04">Abril</option>
+                                    <option value="05">Mayo</option>
+                                    <option value="06">Junio</option>
+                                    <option value="07">Julio</option>
+                                    <option value="08">Agosto</option>
+                                    <option value="09">Septiembre</option>
+                                    <option value="10">Octubre</option>
+                                    <option value="11">Noviembre</option>
+                                    <option value="12">Diciembre</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-xl-3">
+                            <div class="input-group mb-4">
+                                <div class="input-group-append">
+                                    <label class="input-group-text" for="inputGroupSelect02">Año :</label>
+                                </div>
+                                <select class="custom-select" id="inputGroupSelect02">
+                                    <option value="" selected disabled>- Año -</option>
+                                    <?php  for($i=1990;$i<=2020;$i++) { echo "<option value='".$i."'>".$i."</option>"; } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-xl-6"></div>
+                        <div class="col-xl-12">
+                            <h5>Dirección</h5>
+                        </div>
+                        <div class="col-xl-4">
+                            <div class="input-group mb-2">
+                                <div class="input-group-append">
+                                    <label class="input-group-text" for="inputGroupSelect02">Región :</label>
+                                </div>
+                                <select class="custom-select" id="inputGroupSelect02">
+                                    <option selected disabled>- Región -</option>
+                                    <option value="15">Región de Arica y Parinacota</option>
+                                    <option value="1">Región de Tarapacá</option>
+                                    <option value="2">Región de Antofagasta</option>
+                                    <option value="3">Región de Atacama</option>
+                                    <option value="4">Región de Coquimbo</option>
+                                    <option value="5">Región de Valparaíso</option>
+                                    <option value="13">Región de Metropolitana de Santiago</option>
+                                    <option value="6">Región de Libertador General Bernardo O'Higgins</option>
+                                    <option value="7">Región de Maule</option>
+                                    <option value="16">Región de Ñuble</option>
+                                    <option value="8">Región de Biobío</option>
+                                    <option value="9">Región de La Araucanía</option>
+                                    <option value="14">Región de Los Ríos</option>
+                                    <option value="10">Región de Los Lagos</option>
+                                    <option value="11">Región de Aysén del General Carlos Ibáñez del Campo</option>
+                                    <option value="12">Región de Magallanes y de la Antártica Chilena</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-xl-4">
+                            <div class="input-group mb-2">
+                                <div class="input-group-append">
+                                    <label class="input-group-text" for="inputGroupSelect02">Comuna :</label>
+                                </div>
+                                <input type="text" name="comuna" value="" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        <div class="col-xl-4"></div>
+                        <div class="col-xl-6">
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1">Calle :</span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        <div class="col-xl-3">
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1">N° calle # :</span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        <div class="col-xl-12 mb-3" style="margin-top:30px;">
+                            <input type="submit" class="btn btn-outline-success btn-lg btn-block" value="Registrarse">
+                        </div>
                     </div>
-                </div> 
-                <div class="col-xl-12">
-                    <h5>asdasd</h5>  
                 </div>
-            </div>
-            <div class="col-xl-3 borde-r">Derecha</div>
+            </div> <!--Fin de Centro de pagina -->
+            <div class="col-xl-3"></div>
         </div>
     </div>        
 <!-- *******************************************|1 TERMINA EL CONTENIDO |*********************************************************************************************************-->    
